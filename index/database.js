18b13e5247cfb16c2d6b6925f3a241f542aa8111
mateo serebrinsky <mserebrinsky@mail.austral.edu.ar>
2025-03-28 12:08:32 -0300

    agrego boceto de login

--- index/database.js
+++ index/database.js
@@ -12,4 +12,6 @@ database.connect()
     .then(() => console.log('Conectado a la base')) //Manda a la consola si me conecte
     .catch(err => console.error('Error de conexión, no se pudo conectar', err)); //Atrapa el error si no se conecta
 
+//UsersStructure = (id, username, password, email, rank_points)-> id y rank_points se setan automaticamente
+
 module.exports = database;
