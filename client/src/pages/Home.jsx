0e8d6a03ca8a85eb8a51cddf12f4b463f9de6b4e
mateo serebrinsky <mserebrinsky@mail.austral.edu.ar>
2025-04-07 16:38:06 -0300

    Manejo el tema del logOut desde el home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,31 @@
-
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const navigate = useNavigate();
 
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('user'));
+        if (!user) {
+            navigate('/login');
+        }
+    }, [navigate]);
 
+    const handleLogOut = () => {
+        localStorage.removeItem('user')
+        navigate('/login')
+    }
 
-
-
+    return(
+        <div>
+            <h1>
+                hola entraste a la pagina home
+            </h1>
+            <button onClick={handleLogOut}>LogOut</button>
+        </div>
+    )
 
 };
+
+
 export default Home;
