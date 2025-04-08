32708616fb3b58742229537b738969423247db36
mateo serebrinsky <mserebrinsky@mail.austral.edu.ar>
2025-04-07 21:17:14 -0300

    Agrego style a los botones del desplegable de home y agrego un navegate para un futuro implementar la modificacion del profile

--- client/src/pages/BurgerMenu.jsx
+++ client/src/pages/BurgerMenu.jsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import Hamburger from 'hamburger-react'
 import { useNavigate } from 'react-router-dom';
+import styles from './css/LogIn.module.css';
 
 export const BurgerMenu = () => {
 
@@ -11,6 +12,9 @@ export const BurgerMenu = () => {
         localStorage.removeItem('user')
         navigate('/login')
     }
+    const handleClickOnEditProfile = () => {
+        navigate('profile')
+    }
 
     return (
         <div>
@@ -20,8 +24,8 @@ export const BurgerMenu = () => {
                 toggle={setOpen}
             />
             {isOpen && <div>
-                <div>Edit profile</div>
-                <div onClick={handleLogOut}>Log out</div>
+                <div><button className={styles.buttonForDesplegableMenu} onClick={handleClickOnEditProfile}>Edit profile</button></div>
+                <div><button onClick={handleLogOut} className={styles.buttonForDesplegableMenu}>Log out</button></div>
             </div>}
         </div>
     )
