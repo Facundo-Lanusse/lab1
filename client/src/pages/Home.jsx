1014cad25fb9c00d94476cd8ccc7bf5b36c55c0c
mateo serebrinsky <mserebrinsky@mail.austral.edu.ar>
2025-04-13 12:23:47 -0300

    Cambio una mayus que estaba buguenado

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,6 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {BurgerMenu} from "./BurgerMenu";
-
-import styles from "./css/burgerMenu.module.css";
 import styles2 from "./css/GamePlay.module.css";
 
 
@@ -28,7 +26,7 @@ const Home = () => {
             </h1>
             <BurgerMenu/>
 
-            <h1  className={styles.titleDePractica} >Welcome {userName}</h1>
+            <h1  className={styles2.titleDePrueba} >Welcome {userName}</h1>
             <button className={styles2.buttonAnswers} onClick={() => navigate('/play')}>PLAY</button>
         </div>
 
